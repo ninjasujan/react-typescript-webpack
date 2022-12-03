3c020173f12f42bafd05e82ba3e2c1d606e84af1
sujan <sujanb.poojary@gmail.com>
2022-12-03 16:21:33 +0530

    prettier formatter added

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,8 @@ module.exports = {
     "plugin:import/typescript",
     "plugin:jsx-a11y/recommended",
     "plugin:eslint-comments/recommended",
-    // "prettier/@typescript-eslint",
-    // "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   rules: {
     "no-unused-vars": "off",
@@ -30,4 +30,4 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
   },
-};
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import reactImage from "./react.png";
-import svgImage from "./image.svg";
-import ClickCounter from "./ClickCounter";
+import reactImage from "./react.png"
+import svgImage from "./image.svg"
+import ClickCounter from "./ClickCounter"
 
 const App = () => {
   return (
     <div>
       <h1>Typescript babel webpack setup</h1>
+
       <img src={reactImage} width="200" height="200" alt="xasasd" />
       <img src={svgImage} width="200" height="200" alt="adasdas" />
       <ClickCounter />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
