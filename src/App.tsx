a16aa72e670df6ae91e96b08cd28e9181acea261
sujan <sujanb.poojary@gmail.com>
2022-12-03 16:13:37 +0530

    eslint config added

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <div>
       <h1>Typescript babel webpack setup</h1>
-      <img src={reactImage} width="200" height="200" />
-      <img src={svgImage} width="200" height="200" />
+      <img src={reactImage} width="200" height="200" alt="xasasd" />
+      <img src={svgImage} width="200" height="200" alt="adasdas" />
       <ClickCounter />
     </div>
   );
